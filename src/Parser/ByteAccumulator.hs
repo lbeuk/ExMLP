04f475d14b048ef85f701119ee5333604028baaf
Luke Beukelman <lbeuk@proton.me>
2024-01-12 00:07:19 +0000

    Some intermitent changes

--- src/Parser/ByteAccumulator.hs
+++ src/Parser/ByteAccumulator.hs
@@ -3,16 +3,34 @@ module Parser.ByteAccumulator where
 import Data.ByteString.Lazy as BS (length)
 import Data.Int (Int64)
 import Data.Sequence (fromList)
-import Parser ( ParserFn, makeHandle )
+import Parser ( ParserFn, makeHandle, LinkageAssistedParserFn )
 import Data.ParserInternals.LinkedData (LinkedParsableSegment(LinkedParsableSegment, segment), LinkedParseResult (LinkedParseResult))
 import Data.ParserInternals.ParserHandle ( ParserHandle )
 import Data.ParserInternals.ParsableSegment
     ( ParsableSegment(bytes) )
+import Data.ParserInternals.ParseResult
+    ( ParseResult(ParseResult) )
+import Data.ParserInternals.LinkageHelper ( LinkageHelper(..) )
 
 -- | Basic example parser that accumulates the number of bytes read
 byteAccumulatorParser :: ParserHandle
 byteAccumulatorParser = makeHandle byteAccumulatorParserFn 0
 
-byteAccumulatorParserFn :: ParserFn Int64
-byteAccumulatorParserFn st (LinkedParsableSegment src segment) =
-  (LinkedParseResult (fromList []) (fromList []), Just ( st + BS.length (bytes segment)))
+
+byteAccumulatorParser' :: LinkageAssistedParserFn Int64
+byteAccumulatorParser' st segment = 
+  (ParseResult (fromList []) (fromList []), Just ( st + BS.length (bytes segment)))
+
+byteAccumulatorParserFn :: ParserFn (LinkageHelper Int64)
+byteAccumulatorParserFn oldOuterSt linkedSeg =
+  let (res, newInnerSt) = byteAccumulatorParser' (dat oldOuterSt) (segment linkedSeg)
+      newLinkage = linkedSeg <| linked oldOuterSt
+      newOuterSt = case newInnerSt of
+        Nothing -> Nothing
+        Just newInnerSt' -> LinkageHelper {
+          linked = newLinkage,
+          dat = newInnerSt'
+        }
+      
+ linkParseResult :: ParseResult -> Seq LinkedParsableSegment -> (Linked.LinkedParseResult, Bool)
+ linkParseResult ParseResult{}
